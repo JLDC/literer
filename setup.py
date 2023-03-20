f7031753f2fac66744fa245443728475e926bdf3
Jonathan Chassot <JLDC@users.noreply.github.com>
2023-03-20 16:20:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
 setup(
     name="literer",
     version="0.1.0",
     license="MIT",
     description="literer is a package that combines the Semantic Scholar and OpenAI APIs to create automated literature reviews",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Jonathan Chassot",
     author_email="jonathan.chassot@unisg.ch",
     url="https://github.com/JLDC/literer",
